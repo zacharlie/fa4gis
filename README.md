1783309af530ae39476b33ce900d794667bffbcb
Charlie <64078329+zacharlie@users.noreply.github.com>
2020-07-28 23:44:47 +0200

    Initialised
    Created repo for font awesome icons suitable for use with the QGIS resource sharing plugin

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # fa4gis
-Process the Font Awesome Icons for use with the QGIS Resource Sharing Plugin
+
+Font Awesome Icons placed into directory structure for use with the QGIS Resource Sharing Plugin.
+
+Install the [QGIS resource sharing plugin](https://qgis-contribution.github.io/QGIS-ResourceSharing/) and add the URL ```gttps://github.com/zacharlie/fa4gis.git``` as a repository. Once installed, you should have access to the Font Awesome library to use as SVG Symbols.
---
+++ parameterise.py
@@ -0,0 +1,38 @@
+import os
+import xml.etree.ElementTree as ET
+
+# Add qgis symbol parameters to SVG files
+
+# Built on the work of raymondnijssen https://github.com/IFRCGo/IFRC-Icons/blob/master/OCHA_Icons_2018/scripts/convert_to_qgis.py
+
+root_dir = os.path.dirname(os.path.realpath(__file__))
+svg_dir = os.path.join(root_dir, 'collections/font_awesome/svg')
+
+svgNs = 'http://www.w3.org/2000/svg'
+ET.register_namespace('', svgNs)
+
+tags = ('path', 'polygon', 'circle', 'rect')
+uriPrefixedTags = ['{{{0}}}'.format(svgNs) + s for s in tags]
+
+qgisStylingAttributes = {'fill': 'param(fill)',
+                     'stroke': 'param(outline)',
+                     'stroke-width': 'param(outline-width) 0'}
+
+for dirpath, dirs, files in os.walk(svg_dir):
+    for f in files:
+        filename = os.path.join(dirpath, f)
+        try:
+            if filename.endswith(".svg"): 
+                tree = ET.parse(filename)
+                root = tree.getroot()
+                for element in root.iter():
+                    if element.tag in uriPrefixedTags:
+                        element.attrib.pop('class', None)
+                        element.attrib.update(qgisStylingAttributes)
+
+                tree.write(filename, encoding='utf-8')
+                print(filename)
+
+        except Exception as err:
+            print(err)
+
